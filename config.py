dda7f8495bc9416ad769e1956eecfbd8ed556cb6
Morgarinka <freeswolf@gmail.com>
2025-08-20 20:07:35 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import ApplicationBuilder
-import os
-
-TOKEN =("8460331523:AAEqOP8SBmzy-fFeyZ09qfPUoP05wfd-HxQ")
-app = (
-    ApplicationBuilder()
-    .token("8460331523:AAEqOP8SBmzy-fFeyZ09qfPUoP05wfd-HxQ")
-    .build()  
-)
